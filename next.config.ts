ae646595376231c42c9c1d6e4b116044752cbf8c
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-09-07 21:41:22 +0100

    clean up

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  output: 'export',
   /* config options here */
   // webpack: (config, options) => {
   //   if (!options.isServer) {
