e17b7ab24ea37f4e6d08fb58ee20961feca2b1d6
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2025-12-06 00:09:33 +0000

    vendor updated

--- src/components/PlitziBuilderWrapper.tsx
+++ src/components/PlitziBuilderWrapper.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 'use client';
